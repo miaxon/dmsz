ba2119f61b1b2678a821a485dfd552ea2d6fd0e1
conalex <alexcon314@gmail.com>
2017-04-13 16:31:42 +0300

    add reactor

--- logger/zlogpull.cpp
+++ logger/zlogpull.cpp
@@ -18,16 +18,17 @@ namespace dmsz {
         zlogpull::zlogpull(const zmqpp::endpoint_t& endpoint, int workers) :
         m_endpoint(endpoint),
         m_workers(workers),
-        m_ctx(),
-        m_ready(true) {
-            std::thread t(&dmsz::log::zlogpull::run, this);
+        m_ctx()
+        //call_pull(std::bind(&dmsz::log::zlogpull::c_pull, this))
+        {
+            std::thread t(std::bind(&dmsz::log::zlogpull::run, this));
             t.detach();
         }
 
         zlogpull::~zlogpull() {
             zmqpp::socket s(m_ctx, zmqpp::socket_type::push);
             s.connect(m_endpoint);
-            s.send(zmqpp::signal::stop);            
+            s.send(zmqpp::signal::stop);
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
 
@@ -35,20 +36,30 @@ namespace dmsz {
             m_ctx.set(zmqpp::context_option::io_threads, m_workers);
             zmqpp::socket zpull(m_ctx, zmqpp::socket_type::pull);
             zpull.bind(m_endpoint);
-            while (true) {
-                zmqpp::message msg;
+            zmqpp::message msg;
+            zmqpp::reactor reactor;
+            reactor.add(zpull, std::bind(&dmsz::log::zlogpull::c_pull, this));
+            while(reactor.poll())
+            {                
+                
+            }
+            /*while (true) 
                 zpull.receive(msg);
-                if(msg.is_signal())
+                if (msg.is_signal())
                     break;
                 if (msg.parts())
-                    log(msg);
-            }
+                    route(msg);
+            }*/
         }
 
-        void zlogpull::log(zmqpp::message& msg) const {
+        void zlogpull::route(zmqpp::message& msg) const {
             std::string key = msg.get(0);
             //std::string body = msg.get(1);
             std::cout << key << "\n"; //and a number: " << body << std::endl;
+        }
+
+        void zlogpull::c_pull() {
+            std::cout << "c_pull" << "\n"; 
         }
     }
 }
