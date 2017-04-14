7a4cd4d8f7f660ff43abfe6a0ae40f1485b480b2
conalex <alexcon314@gmail.com>
2017-04-14 15:25:15 +0300

    add reactor

--- bin/main.cpp
+++ bin/main.cpp
@@ -34,50 +34,29 @@
 static std::string tcp_endpoint("tcp://127.0.0.1:33353");
 static std::string ipc_endpoint("ipc://11111111");
 static dmsz::log::zlogpull logpull;
-dmsz::log::zlog_st tcp_log(tcp_endpoint);
-dmsz::log::zlog_st ipc_log(ipc_endpoint);
-dmsz::log::zlog_st inp_log;
 
-dmsz::log::zlog_mt tcp_logm(tcp_endpoint);
-dmsz::log::zlog_mt ipc_logm(ipc_endpoint);
-dmsz::log::zlog_mt inp_logm;
+//#define MULTI_THREAD
 
+void test() {    
 
-enum optionIndex {
-    UNKNOWN, HELP, DAEMON
-};
-const option::Descriptor usage[] = {
-    {UNKNOWN, 0, "", "", option::Arg::None, "USAGE: dmsz [options]\n\n"
-        "Options:"},
-    {HELP, 0, "h", "help", option::Arg::None, "  --help, -h  \tPrint usage and exit."},
-    {DAEMON, 0, "d", "daemon", option::Arg::None, "  --daemon, -d  \tRun as daemon."},
-    {0, 0, 0, 0, 0, 0}
-};
-#define MULTI_THREAD
-
-void test() {
-    
-    dmsz::log::zlog_st& logger = inp_log;
-    dmsz::log::zlog_mt& loggerm = tcp_logm;
     using namespace std;
     using namespace std::chrono;
 
-    unsigned int howmany = 1000000;
+    unsigned int howmany = 100;//0000;
     vector<thread> threads;
     auto start = system_clock::now();
 
 
 #if !defined(MULTI_THREAD)
-    
+    dmsz::log::zlog_mt logger(tcp_endpoint);
     for (unsigned int i = 0; i < howmany; i++) {
         //Has to be customized for every logger
-        logger.info(logger.protostring() + " #" + std::to_string(i));
-        loggerm.info(loggerm.protostring() + " #" + std::to_string(i));
-        ipc_logm.info(ipc_logm.protostring() + " #" + std::to_string(i));
+        INFO("asdasdasd");
     }
 #else    
     int thread_count = 4;
     howmany /= thread_count;
+    //dmsz::log::zlog_mt logger;
     for (int t = 0; t < thread_count; ++t) {
         threads.push_back(std::thread([&] {
             dmsz::log::zlog_st logger;
@@ -85,7 +64,7 @@ void test() {
                 //Has to be customized for every logger
                 //stringstream ss;
                 //ss << std::this_thread::get_id() << " Message #" << i;
-                logger.info("#" + std::to_string(i));
+                //INFO("#" + std::to_string(i));
                         //std::this_thread::sleep_for (std::chrono::nanoseconds(10));
             }
         }));
@@ -113,55 +92,14 @@ void test() {
 int main(int argc, char** argv) {
 
     //getchar();
-    test();
+    //test();
     //getchar();
-    return 0;
-}
-/*dmsz::log::zlog logger(log_endpoint);
-std::getchar();
-
-logger.info("");
-logger.info("sdsdsd");
-logger.info("sdsdsd");
-logger.info("sdsdsd");
-
-INIReader reader("dms.conf");
+    dmsz::log::zlog_st logger(tcp_endpoint);
+    //INFO("dfdf");
+    logger.info("0000000000000");
     
-if (reader.ParseError() < 0) {
-    std::cout << "Can't load 'dms.conf'\n";
-    return 1;
-}
-std::cout << "Config loaded from 'test.ini': version="
-          << reader.GetInteger("protocol", "version", -1) << ", name="
-          << reader.Get("user", "name", "UNKNOWN") << ", email="
-          << reader.Get("user", "email", "UNKNOWN") << ", pi="
-          << reader.GetReal("user", "pi", -1) << ", active="
-          << reader.GetBoolean("user", "active", true) << "\n";
-argc -= (argc > 0);
-argv += (argc > 0); // skip program name argv[0] if present
-option::Stats stats(usage, argc, argv);
-std::vector<option::Option> options(stats.options_max);
-std::vector<option::Option> buffer(stats.buffer_max);
-option::Parser parse(usage, argc, argv, &options[0], &buffer[0]);
+    
+    //getchar();
 
-if (parse.error())
-    return 1;
-
-if (options[HELP] || argc == 0) {
-    option::printUsage(std::cout, usage);
     return 0;
 }
-
-
-for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next()) {
-    std::cout << "Unknown option: " << std::string(opt->name, opt->namelen) << "\n";
-}
-
-for (int i = 0; i < parse.nonOptionsCount(); ++i) {
-    std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
-}
-std::cout << GIT_VERSION << std::endl;
-
-std::getchar();
-return 0;*/
-//}
